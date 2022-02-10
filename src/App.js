e811350c7c2a5d4d9df162350024993712f6c621
yousrik20 <yousri.k.e13@gmail.com>
2022-02-10 22:39:43 +0200

    commit message

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
     <div className="App">
         <Navbar/>
           <Routes>
+            <Route path='/food-order-app' element={<Home/>}/>
             <Route path="/" element={<Home/>}/>  
             <Route path='/menu' element={<Menu/>}/>
             <Route path='/about' element={<About/>}/>
